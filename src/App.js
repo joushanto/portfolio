d39a84f7d5013c6c61c0d817e06e5dc00471b248
joushanto <shanto.jouerder@gmail.com>
2022-11-04 19:09:39 +0600

    react structure prepared

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
 
+import {
+  Header,
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+} from "./containers";
+
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
+import "./App.css";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello React!</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
